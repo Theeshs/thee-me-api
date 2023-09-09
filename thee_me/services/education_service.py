b3fd9dc186981c218d5ae17f0d751ee555e7e833
thee <theekshana.sandaru@gmail.com>
2023-09-09 20:17:30 +0800

    updated

--- thee_me/services/education_service.py
+++ thee_me/services/education_service.py
@@ -1,1 +1,7 @@
+""" service controllers for the educations """
+from sqlalchemy.orm import Session
+from thee_me.handlers.
 
+
+def get_all_educations(db: Session, user_id: int):
+    return list_educations(db, user_id)
